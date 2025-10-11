e82b866d95ab38b8682fd2e9c956ced31137b188
daizihan233 <y066ydg5@duck.com>
2025-10-12 02:17:02 +0800

    add initial implementation with Naive UI components and styles

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,86 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <n-config-provider :theme="theme" :date-locale="dateZhCN" :locale="zhCN" class="full">
+    <n-global-style />
+    <n-message-provider class="full">
+      <n-dialog-provider class="full">
+        <n-space vertical class="center stat">
+          <n-steps :current="current" :status="currentStatus">
+            <n-step
+                title="上传图片"
+            />
+            <n-step
+                title="模型处理"
+            />
+            <n-step
+                title="完成！！"
+            />
+          </n-steps>
+          <n-space>
+            <n-button-group>
+              <n-button @click="prev">
+                <template #icon>
+                  <n-icon>
+                    <MdArrowRoundBack />
+                  </n-icon>
+                </template>
+              </n-button>
+              <n-button @click="next">
+                <template #icon>
+                  <n-icon>
+                    <MdArrowRoundForward />
+                  </n-icon>
+                </template>
+              </n-button>
+            </n-button-group>
+            <n-radio-group v-model:value="currentStatus" size="medium" name="basic">
+              <n-radio-button value="error">
+                Error
+              </n-radio-button>
+              <n-radio-button value="process">
+                Process
+              </n-radio-button>
+              <n-radio-button value="wait">
+                Wait
+              </n-radio-button>
+              <n-radio-button value="finish">
+                Finish
+              </n-radio-button>
+            </n-radio-group>
+          </n-space>
+        </n-space>
+      </n-dialog-provider>
+    </n-message-provider>
+  </n-config-provider>
 </template>
+
+
+<script setup>
+import { computed, ref } from "vue";
+import {
+  zhCN, dateZhCN , NConfigProvider, darkTheme, NDialogProvider, NMessageProvider, useOsTheme, NGlobalStyle
+} from "naive-ui";
+import { MdArrowRoundBack, MdArrowRoundForward } from "@vicons/ionicons4";
+
+const osThemeRef = useOsTheme();
+let theme = computed(() => osThemeRef.value === "dark" ? darkTheme : null);
+
+const currentRef = ref(1);
+const currentStatus = ref("process");
+const current = currentRef;
+
+function next() {
+  if (currentRef.value === null)
+    currentRef.value = 1;
+  else if (currentRef.value >= 3)
+    currentRef.value = null;
+  else currentRef.value++;
+}
+
+function prev() {
+  if (currentRef.value === 0)
+    currentRef.value = null;
+  else if (currentRef.value === null)
+    currentRef.value = 3;
+  else currentRef.value--;
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    modules: ['nuxtjs-naive-ui'],
+    build: {
+        transpile: ['naive-ui','vueuc']
+    },
+    vite: {
+        plugins: [
+            tsconfigPaths(),
+            AutoImport({
+                imports: [
+                    {
+                        'naive-ui': [
+                            'useDialog',
+                            'useMessage',
+                            'useNotification',
+                            'useLoadingBar'
+                        ]
+                    }
+                ]
+            }),
+            Components({
+                resolvers: [NaiveUiResolver()]
+            })
+        ]
+    },
+    css: [new URL('./assets/main.css', import.meta.url).pathname]
 })
