c2a581989a4c655f887af9746f555087289a5d89
daizihan233 <y066ydg5@duck.com>
2025-10-12 02:35:57 +0800

    refactor app.vue and add index.vue for image upload functionality

--- app/app.vue
+++ app/app.vue
@@ -3,51 +3,25 @@
     <n-global-style />
     <n-message-provider class="full">
       <n-dialog-provider class="full">
-        <n-space vertical class="center stat">
-          <n-steps :current="current" :status="currentStatus">
-            <n-step
-                title="上传图片"
-            />
-            <n-step
-                title="模型处理"
-            />
-            <n-step
-                title="完成！！"
-            />
-          </n-steps>
-          <n-space>
-            <n-button-group>
-              <n-button @click="prev">
-                <template #icon>
-                  <n-icon>
-                    <MdArrowRoundBack />
-                  </n-icon>
-                </template>
-              </n-button>
-              <n-button @click="next">
-                <template #icon>
-                  <n-icon>
-                    <MdArrowRoundForward />
-                  </n-icon>
-                </template>
-              </n-button>
-            </n-button-group>
-            <n-radio-group v-model:value="currentStatus" size="medium" name="basic">
-              <n-radio-button value="error">
-                Error
-              </n-radio-button>
-              <n-radio-button value="process">
-                Process
-              </n-radio-button>
-              <n-radio-button value="wait">
-                Wait
-              </n-radio-button>
-              <n-radio-button value="finish">
-                Finish
-              </n-radio-button>
-            </n-radio-group>
+        <n-flex vertical class="center stat">
+          <n-space vertical>
+            <n-steps :current="current" :status="currentStatus">
+              <n-step
+                  title="上传图片"
+              />
+              <n-step
+                  title="图片裁剪"
+              />
+              <n-step
+                  title="模型处理"
+              />
+              <n-step
+                  title="完成！！"
+              />
+            </n-steps>
           </n-space>
-        </n-space>
+          <NuxtPage />
+        </n-flex>
       </n-dialog-provider>
     </n-message-provider>
   </n-config-provider>
@@ -59,7 +33,6 @@ import { computed, ref } from "vue";
 import {
   zhCN, dateZhCN , NConfigProvider, darkTheme, NDialogProvider, NMessageProvider, useOsTheme, NGlobalStyle
 } from "naive-ui";
-import { MdArrowRoundBack, MdArrowRoundForward } from "@vicons/ionicons4";
 
 const osThemeRef = useOsTheme();
 let theme = computed(() => osThemeRef.value === "dark" ? darkTheme : null);
@@ -71,7 +44,7 @@ const current = currentRef;
 function next() {
   if (currentRef.value === null)
     currentRef.value = 1;
-  else if (currentRef.value >= 3)
+  else if (currentRef.value >= 4)
     currentRef.value = null;
   else currentRef.value++;
 }
@@ -80,7 +53,7 @@ function prev() {
   if (currentRef.value === 0)
     currentRef.value = null;
   else if (currentRef.value === null)
-    currentRef.value = 3;
+    currentRef.value = 4;
   else currentRef.value--;
 }
 </script>
---
+++ app/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <n-upload
+      multiple
+      directory-dnd
+      action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
+      :max="5"
+  >
+    <n-upload-dragger>
+      <div style="margin-bottom: 12px">
+        <n-icon size="48" :depth="3">
+          <Archive />
+        </n-icon>
+      </div>
+      <n-text style="font-size: 16px">
+        点击或者拖动文件到该区域来上传
+      </n-text>
+      <n-p depth="3" style="margin: 8px 0 0 0">
+        图片不会上传服务器，所有计算与处理在本地进行
+      </n-p>
+    </n-upload-dragger>
+  </n-upload>
+</template>
+
+<script setup>
+import { Archive } from "@vicons/ionicons5";
+import { NIcon, NUpload, NUploadDragger, NText, NP } from "naive-ui";
+</script>
